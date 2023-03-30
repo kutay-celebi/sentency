d1c5175d0009b49e5b3bbb0c7cb8413fc360a1a9
kutay-celebi <celebikutay@gmail.com>
2023-03-30 17:53:05 +0300

    initialize list component

---
+++ ui/src/components/core/SntList.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import SntLoading from "@/components/core/SntLoading.vue";
+</script>
+<template>
+  <ul>
+    <snt-loading></snt-loading>
+    <slot></slot>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+</style>
---
+++ ui/src/components/core/SntListItem.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup></script>
+<template>
+  <li class="snt-list-item">
+    <div>
+      <slot></slot>
+    </div>
+    <div>
+      <slot name="action"></slot>
+    </div>
+  </li>
+</template>
+
+<style lang="scss" scoped>
+.snt-list-item {
+  display: flex;
+  border: 1px solid var(--color-border-main);
+  border-radius: 4px;
+  padding: 0.25rem;
+  margin: 0.125rem 0;
+  cursor: pointer;
+  transition: 0.3s ease background-color;
+  &:hover {
+    background-color: var(--color-hover);
+  }
+}
+</style>
