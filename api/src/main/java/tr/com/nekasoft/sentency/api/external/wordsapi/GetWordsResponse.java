cb0296bc8873ad8c20c89c33e31f310e8d303095
kutay-celebi <celebikutay@gmail.com>
2023-04-03 02:25:59 +0300

    enhance word resource tests

--- api/src/main/java/tr/com/nekasoft/sentency/api/external/wordsapi/GetWordsResponse.java
+++ api/src/main/java/tr/com/nekasoft/sentency/api/external/wordsapi/GetWordsResponse.java
@@ -3,6 +3,7 @@ package tr.com.nekasoft.sentency.api.external.wordsapi;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.Setter;
 import tr.com.nekasoft.sentency.api.entity.Word;
 import tr.com.nekasoft.sentency.api.entity.WordDefinition;
 import tr.com.nekasoft.sentency.api.entity.WordDefinitionExamples;
+import tr.com.nekasoft.sentency.api.exception.ExceptionCode;
 
 @Getter
 @Setter
@@ -27,8 +29,14 @@ public class GetWordsResponse implements Serializable {
   private BigDecimal frequency;
 
   public Set<WordDefinition> toWordDefinitionEntity(Word word) {
+
+    if (results == null) {
+      throw ExceptionCode.DATA_NOT_FOUND.toException(Map.of("word", word.getWord()));
+    }
+
     return results.stream().map(def -> {
-      WordDefinition wordDefinition = WordDefinition.builder()
+      WordDefinition wordDefinition = WordDefinition
+          .builder()
           .word(word)
           .partOfSpeech(def.getPartOfSpeech())
           .definition(def.getDefinition())
@@ -40,12 +48,10 @@ public class GetWordsResponse implements Serializable {
       }
 
       if (def.getExamples() != null) {
-        Set<WordDefinitionExamples> examples = def.getExamples()
+        Set<WordDefinitionExamples> examples = def
+            .getExamples()
             .parallelStream()
-            .map(ex -> WordDefinitionExamples.builder()
-                .example(ex)
-                .definition(wordDefinition)
-                .build())
+            .map(ex -> WordDefinitionExamples.builder().example(ex).definition(wordDefinition).build())
             .collect(Collectors.toSet());
         wordDefinition.setExamples(examples);
       }
--- api/src/test/java/tr/com/nekasoft/sentency/api/endpoint/WordResourceTest.java
+++ api/src/test/java/tr/com/nekasoft/sentency/api/endpoint/WordResourceTest.java
@@ -1,18 +1,35 @@
 package tr.com.nekasoft.sentency.api.endpoint;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
 import io.restassured.response.ValidatableResponse;
+import java.util.Collections;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import tr.com.nekasoft.sentency.api.AbstractWordTestSuite;
 import tr.com.nekasoft.sentency.api.entity.Word;
+import tr.com.nekasoft.sentency.api.external.google.GoogleTranslationExternalService;
+import tr.com.nekasoft.sentency.api.external.wordsapi.Definition;
+import tr.com.nekasoft.sentency.api.external.wordsapi.GetWordsResponse;
+import tr.com.nekasoft.sentency.api.external.wordsapi.WordsApiService;
 
 @QuarkusTest
 class WordResourceTest extends AbstractWordTestSuite {
+
+  @InjectMock
+  @RestClient
+  WordsApiService wordsApiService;
+
+  @InjectMock
+  GoogleTranslationExternalService googleTranslationExternalService;
 
   @Nested
   @TestHTTPEndpoint(WordResource.class)
@@ -24,8 +41,7 @@ class WordResourceTest extends AbstractWordTestSuite {
       Word expected = saveWord();
 
       // when
-      ValidatableResponse actual = given().when().get("/id/{id}", expected.getId()).then().log()
-          .all();
+      ValidatableResponse actual = given().when().get("/id/{id}", expected.getId()).then().log().all();
 
       // then
       actual.statusCode(200);
@@ -33,5 +49,54 @@ class WordResourceTest extends AbstractWordTestSuite {
 
     }
   }
+
+
+  @Nested
+  @TestHTTPEndpoint(WordResource.class)
+  class SearchWord {
+
+    @Test
+    void success() {
+      // given
+      Definition definition = Definition
+          .builder()
+          .definition("definition")
+          .synonyms(Collections.singletonList("synonyms"))
+          .examples(Collections.singletonList("example"))
+          .build();
+      GetWordsResponse response = GetWordsResponse.builder().results(Collections.singletonList(definition)).build();
+      when(wordsApiService.getWord("test")).thenReturn(response);
+
+      when(googleTranslationExternalService.translate(any())).thenReturn("translated");
+
+      // when
+      ValidatableResponse actual = given().when().get("/{word}", "test").then().log().all();
+
+      // then
+      actual.statusCode(200);
+      actual.body("word", equalTo("test"));
+      actual.body("definitions.definition", contains("definition"));
+      actual.body("definitions.definitionTr", contains("translated"));
+      actual.body("definitions[0].synonyms", contains("synonyms"));
+      actual.body("definitions[0].examples", contains("example"));
+
+    }
+
+    @Test
+    void resultsArrayIsNull() {
+      // given
+      GetWordsResponse response = GetWordsResponse.builder().build();
+      when(wordsApiService.getWord("test2")).thenReturn(response);
+
+      // when
+      ValidatableResponse actual = given().when().get("/{word}", "test2").then().log().all();
+
+      // then
+      actual.statusCode(404);
+
+    }
+
+  }
+
 
 }
